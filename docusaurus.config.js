1de8456a90730e3dda1efcfb0eafd1e7d8550825
anders-wiggers <contact@anrs.dk>
2023-08-08 14:58:26 +0200

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,78 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Tour de Azeroth',
+  tagline: '',
+  favicon: 'img/tda.png',
+
+  // Set the production url of your site here
+  url: 'https://tda.anrs.dk',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'tda', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/tda.png',
+      navbar: {
+        title: 'Tour de Azeroth',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/tda.png',
+        },
+      },
+      footer: {
+        logo: {
+          alt: 'Meta Open Source Logo',
+          src: 'img/tda.png',
+          href: 'https://anrs.dk',
+          height: 50
+        },
+        copyright: `Copyright © ${new Date().getFullYear()} Anders Wiggers, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
